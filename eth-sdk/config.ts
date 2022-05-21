7cf9f96f57218ed66c8f04d4b8b2896ba7cf8ae3
Psilon <pzixel@gmail.com>
2022-05-21 10:58:05 +0400

    initial commit

---
+++ eth-sdk/config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from '@dethcrypto/eth-sdk'
+
+export default defineConfig({
+  contracts: {
+    mainnet: {
+      curveRegistry3: '0xB9fC157394Af804a3578134A6585C0dc9cc990d4',
+    },
+  },
+})
